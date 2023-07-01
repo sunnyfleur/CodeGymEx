89436d23df7edf7bb39db28d7db2890888ff32cc
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-01 21:20:20 +0700

    AddLess03

---
+++ CodeGymOnline_Less2_Ex03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less2_Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            float area;
+
+            Console.WriteLine("Enter Width: ");
+            width = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            height = Convert.ToInt64(Console.ReadLine());
+
+            area = width * height;
+            Console.WriteLine("Area is: " + area);
+            Console.ReadKey();
+
+
+        }
+    }
+}
