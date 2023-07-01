a6686761f21878be5f0df2766997d05c198850fa
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-01 21:23:32 +0700

    Addd

---
+++ CodeGymOnline_Less02_Ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less02_Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
