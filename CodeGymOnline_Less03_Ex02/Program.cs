487111bd7b7d5ad5bb9ab5e7106284d9573703be
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-03 16:22:45 +0700

    Add

---
+++ CodeGymOnline_Less03_Ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less03_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight, height,BMI;
+            Console.Write("Enter Your Weight: ");
+            weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Your Height: ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            BMI = weight / Math.Pow(height,2);
+            BMI=Math.Round(BMI);
+            Console.WriteLine("BMI: " + BMI);
+
+            if (BMI < 18.5) Console.WriteLine("Underweight");
+            else if (BMI < 25) Console.WriteLine("Normal");
+            else if (BMI < 30) Console.WriteLine("Overweight");
+            else Console.WriteLine("Obese");
+
+            Console.ReadKey();
+
+            
+
+
+        }   
+    }
+}
---
+++ CodeGymOnline_Less03_Ex03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less03_Ex03
+
+{
+    public class ReadNumber
+    {
+      private string[] units = {"zero", "one", "two", "three", "four",
+                                "five", "six", "seven", "eight", "nine"};
+        private string[] tys = { "", "ten", "twenty", "thirty", "forty",
+                                "fifty", "sixty", "seventy", "eighty", "ninety"};
+        private string[] teens = { "ten", "eleven", "twelve", "thirteen", "fourteen",
+                                   "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+        public string Read(int number)
+        {
+            if (number < 10)
+                return units[number];
+            if (number < 20)
+                return teens[number - 10];
+            if (number < 100)
+                return tys[number / 10] + ((number % 10 != 0) ? " " : "") + units[number % 10];
+            if (number < 1000)
+                return units[number / 100] + " hundred" + ((number % 100 != 0) ? " " : "") + Read(number % 100);
+            else return "Out of limited input";
+        }
+    }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber;
+            string outputRead;
+            Console.Write("Enter Number To Read: ");
+            inputNumber =Convert.ToInt32( Console.ReadLine());
+
+            ReadNumber read = new ReadNumber();          
+            outputRead=read.Read(inputNumber);
+            Console.WriteLine("It is: "+ outputRead);
+            Console.ReadKey();
+        }
+    }
+}
