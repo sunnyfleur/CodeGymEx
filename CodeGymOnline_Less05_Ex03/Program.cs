b2d4f2624e1a03f8e29fb371e8aacbd0ada57479
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-08 08:35:27 +0700

    03

---
+++ CodeGymOnline_Less05_Ex03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less05_Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            int[] array;
+
+            do
+            {
+                Console.Write("Enter a size for array: ");
+                size = Convert.ToInt32(Console.ReadLine());
+                if (size > 20) 
+                    Console.WriteLine("Size can not exceed 20 value");
+            }
+            while (size > 20);
+
+            array = new int[size];
+
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.Write("Enter element " + (i + 1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("Property list: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j] + "\t");
+            }
+
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    index = j + 1;
+                }
+            }
+            Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
+
+            Console.ReadKey();
+        }
+    }
+}
