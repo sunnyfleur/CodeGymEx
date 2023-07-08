37fdabbc8ea7284d39fdba499fe5f6a41cb71dd9
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-08 08:24:27 +0700

    02

--- CodeGymOnline_Less05_Ex02/Program.cs
+++ CodeGymOnline_Less05_Ex02/Program.cs
@@ -10,6 +10,24 @@ namespace CodeGymOnline_Less05_Ex02
     {
         static void Main(string[] args)
         {
+            string inputName;
+            bool isExist=false;
+
+            string[] students = { "Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily" };
+            Console.Write("Enter a name’s student: ");
+            inputName = Console.ReadLine();
+
+            for(int i=0; i < students.Length; i++)
+            {
+                if (inputName == students[i])
+                {
+                    Console.WriteLine("Position of the students in the list " + inputName + " is: " + (i + 1));
+                    isExist = true; break;
+                }
+            }
+
+            if (!isExist) Console.WriteLine("Not found " + inputName + " in the list.");
+            Console.ReadKey();
         }
     }
 }
