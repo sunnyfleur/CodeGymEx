827a295407bfb99b2500e8acb90642823c393893
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-12 01:21:10 +0700

    adad

---
+++ CodeGymOnline_Less06_Ex03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less06_Ex03
+{
+    internal class Program
+    {
+        public static int CheckCharacter(string inputString, char inputChar)
+        {
+            int count=0;
+            foreach(char c in inputString)
+            {
+                if(c == inputChar)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputString;
+            char inputChar;
+            int count;
+
+            Console.Write("Enter your string: ");
+            inputString = Console.ReadLine();
+            Console.Write("Enter your character: ");
+            inputChar= Convert.ToChar(Console.ReadLine());
+
+            count=CheckCharacter(inputString, inputChar);
+
+            Console.Write(inputChar + " appear " + count + " times in " + inputString);
+            Console.ReadKey();
+
+
+        }
+    }
+}
