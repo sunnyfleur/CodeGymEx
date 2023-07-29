88889f934420535b2753ee68d3781cb7a00ca92f
Tran Hoang Kiet <trabhoangkiettcag@gmail.com>
2023-07-29 19:30:41 +0700

    aa

---
+++ CodeGymOnline_Less12_Ex05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGymOnline_Less12_Ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
